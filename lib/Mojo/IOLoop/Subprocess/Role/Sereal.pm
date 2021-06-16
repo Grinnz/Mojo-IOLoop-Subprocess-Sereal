eca5e11f7dac33794847f2e37b80c97fb3b247a9
Dan Book <grinnz@grinnz.com>
2021-06-15 23:59:24 -0400

    Bump version

--- lib/Mojo/IOLoop/Subprocess/Role/Sereal.pm
+++ lib/Mojo/IOLoop/Subprocess/Role/Sereal.pm
@@ -4,7 +4,7 @@ use Role::Tiny;
 use Sereal::Decoder;
 use Sereal::Encoder;
 
-our $VERSION = '1.002';
+our $VERSION = '1.003';
 
 requires qw(deserialize serialize);
 
--- lib/Mojo/IOLoop/Subprocess/Sereal.pm
+++ lib/Mojo/IOLoop/Subprocess/Sereal.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Exporter 'import';
 
-our $VERSION = '1.002';
+our $VERSION = '1.003';
 
 our @EXPORT = '$_subprocess';
 
